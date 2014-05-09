91f70ceadf4e0bb8f5fa4c767d6a0e89ffb0cabe
Joshua Coutinho <jrc12@imperial.ac.uk>
2014-05-09 18:06:17 +0100

    fixed register machine state monad to use labels as values

--- RegMachineEg.hs
+++ RegMachineEg.hs
@@ -7,25 +7,29 @@ import Data.List (delete)
 data Label  = L Int deriving (Eq, Ord, Show)
 data Reg    = Reg Int deriving (Eq, Ord, Show)
 data Body = Add Reg Label | Sub Reg Label Label
-type State  = (Label, [(Reg,Int)])
+type State  = [(Reg,Int)]
 type Machine = Map Label Body
 type RegisterBindings = Map Int Int
-type RegisterMachine = RdrT Machine (St State) ()
+type RegisterMachine = RdrT Machine (St State) Label
 
 
 relabel :: RegisterBindings -> Machine -> Machine
-relabel = undefined
+relabel rb m = undefined
+
+gadgetReaderT :: RdrT RegisterBindings (RdrT Machine (St State)) Label
+gadgetReaderT = undefined
+--p = ReaderT $ ((ReaderT).(runReaderT regReaderT.)).relabel
 
 
--- transform Normal register machine into gadget using Reader Transformer
-gadgetReaderT :: RdrT RegisterBindings (RdrT Machine (St State)) ()
-gadgetReaderT 
- = ReaderT $ ((ReaderT).(runReaderT regReaderT.)).relabel
+start = return (L 0) :: RegisterMachine
+
+step :: Label -> RegisterMachine
+step = ReaderT.((State.).executeInstr)
+
+runReg = (runState.).runReaderT 
 
 
 
-regReaderT :: RdrT Machine (St State) ()
-regReaderT = ReaderT (State . executeInstr)
 
 update :: (Int -> Int) -> Reg -> [(Reg, Int)] -> [(Reg, Int)]
 update f r regs = (r ,f value):(delete (r, value) regs)
@@ -33,21 +37,22 @@ update f r regs = (r ,f value):(delete (r, value) regs)
     (Just value) = Prelude.lookup r regs
 
 
-executeInstr :: Machine -> State -> ((), State)
-executeInstr config (l, regs) = func (M.lookup l config)
+
+executeInstr ::  Label -> Machine -> State -> (Label, State)
+executeInstr l config regs = func (M.lookup l config)
   where
-    func :: Maybe Body -> ((), State)
-    func Nothing  = ((),(L (-1), regs))
-    func (Just (Add reg label)) = ((), (label, update (+1) reg regs))
-    func (Just (Sub reg l l')) = if x == 0 then ((),(l', regs))
-                                           else ((), (l, update ((-)1) reg regs))
+    func :: Maybe Body -> (Label, State)
+    func Nothing  = (L (-1), regs)
+    func (Just (Add reg label)) = (label, update (+1) reg regs)
+    func (Just (Sub reg l l')) = if x == 0 then (l, regs)
+                                           else (l, update ((-)1) reg regs)
       where
        (Just x) = Prelude.lookup reg regs 
 
 
 
 
--------------------------------------------------------------------------------- State Moand Transformer example
+-------------------------------------------------------------------------------- State Monad Transformer example
 
 -- simpleReader :: Rdr Int Bool
 simpleReader = Reader (/= 0)
@@ -63,5 +68,5 @@ program1 :: StT Int (Rdr Int) Bool
 program1
  = (liftST simpleReader) >>= bindF >>= bindF >>= bindF
 
-run :: StT Int (Rdr Int) Bool -> Int -> Int -> (Bool, Int)
-run  = (runReader.).runStateT
+runSimple :: StT Int (Rdr Int) Bool -> Int -> Int -> (Bool, Int)
+runSimple  = (runReader.).runStateT
--- TestRegMachines.hs
+++ TestRegMachines.hs
@@ -7,8 +7,8 @@ import MonadInstances
 --------------------------------------------------------------------------------
 -- TESTS Simple
 
-sTest1 = run program1 0 0 == (False, 3)
-sTest2 = run program1 0 1 == (True, 3)
+sTest1 = runSimple program1 0 0 == (False, 3)
+sTest2 = runSimple program1 0 1 == (True, 3)
 
 testSimple = [sTest1, sTest2]
 
@@ -24,23 +24,28 @@ infiniteLoop = fromList [lb 0 (add 1 1), lb 1 (sub 1 0 0)]
 zero :: Machine
 zero = fromList [lb 0 (sub 1 0 1)]
 
+
+
 -- add bindings for a register machine
 
 
 initialState :: State
-initialState = (L 0, map (flip (,) 0.Reg) [0..])
+initialState = (map (flip (,) 0.Reg) [0..])
 
 add r j = Add (Reg r) (L j)
 sub r j k = Sub (Reg r) (L j) (L k)
 lb = (,).L 
 
 
+testFunc numR
+  = ((fmap (take numR).).) . getResult
 
 
-testFunc numR iters
-  = (take numR.).(snd.).(snd.).runState.(runReaderT (foldl1 (>>) (replicate iters regReaderT)))
+getResult iters = runState.(runReaderT (Prelude.foldl (>>=) start (replicate iters step)))
 
-cTest1 = testFunc 1 5 infiniteLoop initialState == [(Reg 1, 1)]
+
+cTest1 = testFunc 2 0 infiniteLoop initialState == (L 0, [(Reg 0,0), (Reg 1, 0)])
+cTest2 = testFunc 2 1 infiniteLoop initialState == (L 0, [(Reg 1,1), (Reg 0, 0)])
 
 
 testReg = [cTest1]
