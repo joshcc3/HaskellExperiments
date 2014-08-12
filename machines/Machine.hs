286bfd100af39df5880a4a7d209c1e4c3afc3f75
Joshua Coutinho <jrc12@imperial.ac.uk>
2014-08-12 17:27:10 +0100

    Added monoid instance

--- machines/Machine.hs
+++ machines/Machine.hs
@@ -1,14 +1,22 @@
+{-# LANGUAGE TypeSynonymInstances, RankNTypes, FlexibleInstances #-}
+
 module Machine where
 
 import Control.Applicative
 import Control.Comonad
+import Prelude hiding ((.), id)
+import Data.Monoid
+import Control.Monad.Trans.Maybe
 
 data Moore a b = Moore b (a -> Moore a b)
 
+newtype Moore' a b = Moore' (MaybeT (Moore a) b)
 
 instance Functor (Moore a) where
     fmap f (Moore b g) = Moore (f b) (fmap f . g)
-
+        where
+            (.) f g x = f (g x)
+    
 instance Applicative (Moore a) where
     pure x = Moore x $ const $ pure x
     Moore b f <*> Moore b' g = Moore (b b') $ \a -> f a <*> g a
@@ -22,7 +30,16 @@ instance Monad (Moore a) where
 
 instance Comonad (Moore a) where
     extract (Moore b _) = b
-    duplicate m@(Moore b f) = Moore m $  duplicate. f
+    duplicate m@(Moore b f) = Moore m $  duplicate . f
+        where
+        (.) f g x = f (g x)
+
+
+
+
+instance Monoid b => Monoid (Moore a b) where
+    mempty = Moore mempty $ const mempty
+    mappend m m' = liftA2 mappend m m' 
 
 
 
