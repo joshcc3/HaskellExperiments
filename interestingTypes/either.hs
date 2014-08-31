0ede51540f230227cabc15e10f3f99bfd02c5ced
Joshua Coutinho <jrc12@imperial.ac.uk>
2014-08-31 13:50:31 +0100

    Added note on either type

---
+++ interestingTypes/either.hs
@@ -0,0 +1,6 @@
+{-
+Something absolutely beautiful about the either type. So some thoughts. Its a sum type. And paired with the product type it represents something awesome. Firstly isomorphic to (+) (*), then to sequence and if statements in control flow, and also in regular expressions - concatentation and alternation.
+Also denotational semantics seem to be the dual of operational semantics or is that a stretch. What I mean to say is that, when considering properties of regular expressions concatenation behaves like the product and alternation behaves like the sum. However when considering the operational semantics its the reverse. And since they are the duals of each other, my conclusion.
+
+Also lensing into a stream of eithers forms a geometric series, what is the relationship to the geometric series on natural numbers.
+-}
