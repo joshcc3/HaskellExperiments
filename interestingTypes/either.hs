11be66f935a20e9182f2f4e3d7be276be31bdfa3
Joshua Coutinho <jrc12@imperial.ac.uk>
2014-09-07 03:16:52 +0100

    Added notes on intersection types

--- interestingTypes/either.hs
+++ interestingTypes/either.hs
@@ -3,12 +3,15 @@ Something absolutely beautiful about the either type. So some thoughts. Its a su
 Also denotational semantics seem to be the dual of operational semantics or is that a stretch. What I mean to say is that, when considering properties of regular expressions concatenation behaves like the product and alternation behaves like the sum. However when considering the operational semantics its the reverse. And since they are the duals of each other, my conclusion.
 
 Also lensing into a stream of eithers forms a geometric series, what is the relationship to the geometric series on natural numbers.
+
+Subtraction are intersection types. We said that intersection types are formed through class constratins. We can maybe represent the class constraints through conjunctions of functions. for example isomorphisms between some types will only hold for certain conditions on the existing types
 -}
 
 
-data Tree b = Branch b [Tree b]
+data TreeF1 b n = Branch b [n]
 
 {-
+
 list(a) = 1 + a*list(a)
 list(a) = 1/(1 - a)
 tree(a) = a*list(tree(a))
@@ -18,7 +21,8 @@ tree(a) - tree^2(a) = a
 tree(a) = a + tree^2(a)
 -}
 
-data TreeF a n = Leaf a | BranchF n n
+data TreeF2 a n = Leaf a | BranchF n n
 
+-- question: How to define an isomorphism between TreeF1 and TreeF2
 -- degrees of freedom much like in equations. All the theory applicable on numbers should
 -- then apply such as integration, numerical analysis and all other such nonsense.
--- machines/Lang.hs
+++ machines/Lang.hs
@@ -6,7 +6,17 @@ import Data.Fix
 import Mon
 
 data LangF a n
- = Id a | Br n n
+ = Stop | Br n n
+
+
+instance Functor (LangF a) where
 
 data Stat = Print String | Read String
 
+{-
+For the F-Algebra make the carrier set continuations.
+We want to build up continuations for the backtracking 
+computations and we will use an F-Algebra to achieve that.
+-}
+
+newtype Cont r a = Cont { runCont :: (a -> r) -> r }
