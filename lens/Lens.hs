b0ffd051fa511e9a17331e284064b5059496394d
Joshua Coutinho <jrc12@Joshuas-MacBook-Pro.local>
2014-09-27 02:27:00 +0530

    Added small implementation to lens

---
+++ lens/Lens.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+--{-# LANGUAGE ExistentialQuantification #-}
+--{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+--{-# LANGUAGE LambdaCase #-}
+
+module Lens where
+
+import Data.Profunctor
+import Control.Monad.Identity
+import Type
+import Control.Monad.State
+
+type Lens s t a b = Functor f => (a -> f b) ->  s -> f t
+
+type Lens' s a = Lens s s a a
+
+type ALens s t a b = LensLike (Pretext (->) a b) s t a b
+
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens f g v s = fmap (g s) (v $ f s)
+
+(%%~) :: Functor f => Lens s t a b -> (a -> f b) -> s -> f t
+(%%~) f = f
+
+(%%=) :: MonadState s m => Lens s s a b -> (a -> (r, b)) -> m r
+(%%=) l f = case l f of
+              sf -> do
+                     s <- get
+                     case sf s of
+                       (r, s') -> do
+                                 put s'
+                                 return r
--- lens/Type.hs
+++ lens/Type.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Type where
 
@@ -11,3 +12,9 @@ data Exchange a b s t = Exchange (s -> a) (b -> t)
 
 instance Profunctor (Exchange a b) where
     dimap f g (Exchange f' g') = Exchange (dimap f id f') (fmap g g')
+
+
+type LensLike f s t a b = (a -> f b) -> s -> f t
+
+newtype Pretext p a b t = Pretext { runPretext :: Functor f => p a (f b) -> f t }
+
