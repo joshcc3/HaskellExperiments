31ee9f5bdfd43ca2404bc314627feaf4cd7c2c22
Joshua Coutinho <jrc12@imperial.ac.uk>
2015-01-06 21:16:28 +0530

    Fixed html parser

--- yesodExp/Templates/Blog.hs
+++ yesodExp/Templates/Blog.hs
@@ -13,7 +13,8 @@ import Control.Arrow
 import Yesod
 import Text.Blaze.Html
 import Text.Blaze.Renderer.String
-
+import WebScraper
+import Control.Monad
 
 --getHomeR :: Handler Html
 type Year = Int
@@ -24,9 +25,8 @@ type No = Int
 toFile :: Year -> Month -> Day -> No -> FilePath
 toFile y m d n = Prelude.concat ["./blog/", show y, "/", show m,  "/", show d,  "/", show n, ".html"] 
 bArticle :: Year -> Month -> Day -> No -> IO Html
-bArticle y m d n = do
-  inp <- readFile $ toFile y m d n 
-  return $ writeHtml def {writerHtml5 = True } . readHtml def $ inp
+bArticle y m d n = readFile >=> extractHtml $ toFile y m d n 
+
 
 
 bIndex :: IO Html
