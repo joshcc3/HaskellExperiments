f1144c7d65103768e1521ba96a5ea84e40dba12b
Joshua Coutinho <jrc12@imperial.ac.uk>
2015-01-19 23:21:03 +0000

    Started implementation of modal logic primitives

---
+++ edsls/ModalLogic.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module ModalLogic where
+
+import Data.Functor.Foldable
+
+
+data ModalLogicF a n = Id a | Not n | n :|| n | n :&& n | Box n | TrueM
+                     deriving (Functor, Eq, Ord, Show)
+
+type ModalLogic a = Fix (ModalLogicF a)
+
+type KripkeF a = ([a], Set (a, a))
+
+type KripkeM p a = (KripkeF a, p -> Set a)
+
+type Property a = ModalLogic a
+
+diamond :: ModalLogic a -> ModalLogic a
+diamond = Fix . Not . Fix . Box . Fix . Not
+
+false :: ModalLogic a
+false = Fix $ Not (Fix TrueM)
+
+(|=) :: forall p a. (KripkeM p a, a) -> Property p -> Bool
+(m@((universe, r), ef), w) |= property = para alg property
+  where
+    alg :: ModalLogicF p (Property p, Bool) -> Bool
+    alg TrueM = True
+    alg (Id p) = ef p w
+    alg (Not (_, n)) = not n
+    alg ((_, n) :|| (_, n')) = n || n'
+    alg ((_, n) :&& (_, n')) = n && n'
+    alg (Box (n, _)) = all (\w' -> not (r (w, w')) || (m, w') |= n) universe
+
+
+-- this is the meaning of a set
+type Set a = a -> Bool
+
